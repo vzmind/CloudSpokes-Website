1fc1d2e2154c6ed7201534e5ff19e29d491bda30
Vzmind <vzmind@gmail.com>
2011-10-19 22:58:12 +0200

    Retrieve all SFDC fields for members index page

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,6 +3,6 @@ require "members"
 class MembersController < ApplicationController
 
   def index
-    @members = Members.all(:fields => "id,name,email__c")
+    @members = Members.all(:fields => "id,name,email__c,profile_pic__c,summary_bio__c,challenges_entered__c,total_1st_place__c,total_2nd_place__c,total_3st_place__c")
   end
 end
